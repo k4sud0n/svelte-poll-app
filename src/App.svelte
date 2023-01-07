114f567ef09382335830c3c02f688c15a398290f
Sihyun Kim <k4sud0n@gmail.com>
2023-01-07 06:11:16 +0000

    Finish Lesson 4

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script>
   let name = 'Svelte';
+
+  // const handleInput = (e) => {
+  //   name = e.target.value;
+  // }
 </script>
 
 <h1>Hello {name}!</h1>
+<!-- <input type="text" value={name} on:input={handleInput}> -->
+<input type="text" bind:value={name}>
