d166780aaffb62856c79962e987976e71a199d5e
Sihyun Kim <k4sud0n@gmail.com>
2023-01-08 11:17:37 +0000

    Finish Lesson 6

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,19 @@
 <script>
-  let firstName = 'Jimi';
-  let lastName = 'Hendrix';
-  let beltColor = 'black';
-
-  $: fullName = `${firstName} ${lastName}`;
-  $: {
-    console.log(lastName);
-    console.log(beltColor);
-  }
+  let people = [
+    { name: 'yoshi', beltColor: 'black', age: 25 , id: 1},
+    { name: 'mario', beltColor: 'orange', age: 45 , id: 2},
+    { name: 'luigi', beltColor: 'brown', age: 35 , id: 3},
+  ]
 </script>
 
 <main>
-  <p>{fullName} - {beltColor}</p>
-  <input type="text" bind:value={firstName}>
-  <input type="text" bind:value={lastName}>
-  <input type="text" bind:value={beltColor}>
+  {#each people as person (person.id)}
+    <div>
+      <p>{person.name}</p>
+      <p>{person.beltColor}</p>
+      <p>{person.age}</p>
+    </div>
+  {:else}
+    <div>There are no people to show...</div>
+  {/each}
 </main>
