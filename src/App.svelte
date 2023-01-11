921ddb1fadb47d20995e12a9138586c582054fa9
Sihyun Kim <k4sud0n@gmail.com>
2023-01-11 04:53:16 +0000

    Finish Lesson 18

--- src/App.svelte
+++ src/App.svelte
@@ -1,62 +1,7 @@
-<script>
-    import Modal from './Modal.svelte';
-    import AddPersonForm from './AddPersonForm.svelte';
-
-    let showModal = false;
-
-    const toggleModal = () => {
-        showModal = !showModal;
-    }
-
-    let people = [
-        { name: 'yoshi', beltColor: 'black', age: 25 , id: 1},
-        { name: 'mario', beltColor: 'orange', age: 45 , id: 2},
-        { name: 'luigi', beltColor: 'brown', age: 35 , id: 3},
-    ]
-
-    const deletePerson = (id) => {
-        people = people.filter((person) => person.id != id)
-    }
-
-    const addPerson = (e) => {
-        const person = e.detail;
-        people = [person, ...people];
-        showModal = false;
-    }
-</script>
-
-<Modal {showModal} on:click={toggleModal}>
-    <h3>Add a New Person</h3>
-    <AddPersonForm on:addPerson={addPerson} />
-</Modal>
+<script></script>
 
 <main>
-    <button on:click|once={toggleModal}>Open Modal</button>
-
-    {#each people as person (person.id)}
-        <div>
-            <h4>{person.name}</h4>
-            {#if person.beltColor === 'black'}
-              <p><strong>MASTER NINJA</strong></p>
-            {/if}
-            <p>{person.age} years old, {person.beltColor} belt.</p>
-            <button on:click={() => {deletePerson(person.id)}}>delete</button>
-        </div>
-    {:else}
-    	<div>There are no people to show...</div>
-    {/each}
+  hi svelte
 </main>
 
-<style>
-  main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<style></style>
