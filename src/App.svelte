b80aa021472aa559aa3a1ad5ef20c721beef14be
Sihyun Kim <k4sud0n@gmail.com>
2023-01-11 05:35:24 +0000

    Finish Lesson 21

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
 
 	import Tabs from './lib/Tabs.svelte';
 
@@ -20,7 +21,7 @@
 	{#if activeItem === 'Current Polls'}
 		<p>Poll list component goes here</p>
 	{:else if activeItem === 'Add New Poll'}
-		<p>New poll component goes here</p>
+		<CreatePollForm />
 	{/if}
 </main>
 
