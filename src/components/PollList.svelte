e32bb26198eec4fc838dc9f0c9b47abc9b85c1a7
Sihyun Kim <k4sud0n@gmail.com>
2023-01-11 07:26:42 +0000

    Finish Lesson 33

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,12 +1,16 @@
 <script>
     import PollStore from '../stores/PollStore';
+    import { fade, scale } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
 
     import PollDetails from './PollDetails.svelte';
 </script>
 
 <div class="poll-list">
     {#each $PollStore as poll (poll.id)}
-        <PollDetails {poll} />
+        <div in:fade out:scale|local animate:flip={{duration: 500}}>
+            <PollDetails {poll} />
+        </div>
     {/each}
 </div>
 
