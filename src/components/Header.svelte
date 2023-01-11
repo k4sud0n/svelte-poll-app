3c588708b442074ab7c704105571fffae32716d7
Sihyun Kim <k4sud0n@gmail.com>
2023-01-11 05:05:03 +0000

    Finish Lesson 19

---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<header>
+    <h1>
+        <img src="./vite.svg" alt="Poll App Logo">
+    </h1>
+</header>
+
+<style>
+    header {
+        background: #f7f7f7;
+        padding: 20px;
+    }
+
+    h1 {
+        margin: 0;
+        text-align: center;
+    }
+</style>
